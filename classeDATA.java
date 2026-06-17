ea156e10896064348d320f56a6ca717bb099432b
igolima16 <167836742+igolima16@users.noreply.github.com>
2026-06-17 16:35:35 -0300

    Update classeDATA.java

--- classeDATA.java
+++ classeDATA.java
@@ -1,7 +1,33 @@
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Scanner;
+/*
+		aluno: Igo lima borges
+		aluno: Guilherme silvestre de farias 
+	
+		exercicio 1
+		• O construtor Data() deverá permitir ao usuário digitar os valores de dia, mês e ano e com eles
+			inicializar os atributos da classe. Os valores digitados deverão ser consistidos e só aceitos se válidos,
+			caso contrário, redigitar;
+			• O construtor Data(int d, int m, int a) deverá receber os valores de dia, mês e ano e com eles inicializar
+			as propriedades da classe;
+			• Os métodos entraDia(int d), entraMes(int m) e entraAno(int a) devem receber um valor e atribuí-lo às
+			respectivas propriedades;
 
+			• Os métodos entraDia (),entraMes () e entraAno () devem permitir ao usuário digitar um valor e atribuí-
+			lo a respectiva propriedade. Os valores digitados devem sofrer consistência e só aceitos quando válidos,
+
+			caso contrário, solicitar ao usuário redigitar;
+			• Os métodos retDia(), retMes() e retAno() devem nos devolver as respectivas propriedades;
+			• O método mostra1() deve nos devolver a data no formato: dd/mm/aaaa;
+			• O método mostra2() deve nos devolver a data no formato: dd/mesPorExtenso/ano;
+			• O método bissexto() deve nos devolver um boolean informando se o ano é ou não bissexto;
+			• O método diasTranscorridos, deve retornar a quantidade de dias transcorridos no ano até a data
+			digitada.
+			• O método apresentaDataAtual() deve imprimir a data atual, utilizando as classes Date e DateFormat, o
+			DateFormat empregando o seguinte método: getDateInstance(DateFormat.FULL);
+			• Conveniente colocar tratamento de exceção para as possíveis inconsistências na entrada de dados.
+		*/
 public class Data {
     
     private int dia;
